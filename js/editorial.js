e823e7db288248c9e93462127cb959e6bb169a68
최윤경 <rose06166@naver.com>
2025-08-25 18:18:42 +0900

    기존 레이아웃 수정_2차
    비틀즈, typo_expei, v&m_graphic, v&m_typo 수정

--- js/editorial.js
+++ js/editorial.js
@@ -1,3 +1,10 @@
+const cursor = document.querySelector(".custom-cursor");
+
+document.addEventListener("mousemove", (e) => {
+  cursor.style.left = `${e.clientX}px`;
+  cursor.style.top = `${e.clientY}px`;
+});
+
 document.addEventListener("DOMContentLoaded", function () {
   const detailsElements = document.querySelectorAll("details");
   detailsElements.forEach((details) => {
